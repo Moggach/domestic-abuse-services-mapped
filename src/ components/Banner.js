38516c895b4631194ea8e1bb93718a95fe88f117
Moggach <acunnane13@gmail.com>
2024-04-28 19:47:37 +0100

    animate safety banner

--- src/ components/Banner.js
+++ src/ components/Banner.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import { BannerContainer } from '../styles/LayoutStyles';
+import styled, { keyframes } from 'styled-components';
+
+const slideInFromTop = keyframes`
+ from {
+    transform: translateY(-100%);
+    opacity: 0;
+ }
+ to {
+    transform: translateY(0);
+    opacity: 1;
+ }
+`;
+
+const AnimatedBannerContainer = styled(BannerContainer)`
+ animation: ${slideInFromTop} 0.3s ease-out forwards;
+`;
 
 const Banner = ({ onClose }) => {
  const goToGoogle = () => {
@@ -7,13 +23,13 @@ const Banner = ({ onClose }) => {
  };
 
  return (
-    <BannerContainer>
+    <AnimatedBannerContainer>
       <p>If you are in an emergency, please call 999</p>
       <p>If you’re worried someone might be monitoring your devices, exit this website and visit from a device only you have access to. </p>
       <p>Learn more about <a href="https://refugetechsafety.org/">safe browsing, and keeping your technology safe.</a></p>
       <button onClick={goToGoogle}>Quick exit</button>
       <button onClick={onClose}>Close</button>
-    </BannerContainer>
+    </AnimatedBannerContainer>
  );
 };
 
