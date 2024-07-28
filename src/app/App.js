a7d8c15a18649821f331935fb8a675c8051c6f27
Moggach <acunnane13@gmail.com>
2024-07-28 20:06:30 +0100

    replace image link with svg code so it uses current color

--- src/app/App.js
+++ src/app/App.js
@@ -10,7 +10,6 @@ import Footer from './components/Footer';
 import ThemeSwitcher from './components/ThemeSwitcher';
 import ServiceTypeFilter from './components/ServiceTypeFilter';
 import SpecialismCheckboxes from './components/SpecialismCheckboxes';
-import externalLinkIcon from './images/svgs/exernal_link.svg';
 import { calculateDistance, fetchCoordinates, colorMapping, determineZoomLevel } from './utils';
 
 
@@ -155,7 +154,7 @@ const Home = ({ serverAirtableData, initialServiceTypes, initialSpecialisms }) =
 
   return (
     <>
-    
+
       <main className="p-4 lg:flex lg:flex-row-reverse lg:gap-6">
         <Modal />
         <QuickExit />
@@ -175,7 +174,7 @@ const Home = ({ serverAirtableData, initialServiceTypes, initialSpecialisms }) =
 
 
         <div className='flex flex-col gap-5 basis-1/2'>
-        <ThemeSwitcher />
+          <ThemeSwitcher />
 
           <ServiceTypeFilter
             selectedServiceType={selectedServiceType}
@@ -242,11 +241,8 @@ const Home = ({ serverAirtableData, initialServiceTypes, initialSpecialisms }) =
                           )}
                         </div>
                         <a href={properties.website}>
-                          <img
-                            alt="an SVG icon indicating an external link"
-                            src={externalLinkIcon.src}
-                            style={{ width: '20px' }}
-                          />
+                          <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg"><path d="M432,320H400a16,16,0,0,0-16,16V448H64V128H208a16,16,0,0,0,16-16V80a16,16,0,0,0-16-16H48A48,48,0,0,0,0,112V464a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V336A16,16,0,0,0,432,320ZM488,0h-128c-21.37,0-32.05,25.91-17,41l35.73,35.73L135,320.37a24,24,0,0,0,0,34L157.67,377a24,24,0,0,0,34,0L435.28,133.32,471,169c15,15,41,4.5,41-17V24A24,24,0,0,0,488,0Z"></path></svg>
+
                         </a>
                       </div>
                     </div>
@@ -265,11 +261,7 @@ const Home = ({ serverAirtableData, initialServiceTypes, initialSpecialisms }) =
                         <h3 className="card-title">{properties.name}</h3>
 
                         <a href={properties.website}>
-                          <img
-                            alt="an SVG icon indicating an external link"
-                            src={externalLinkIcon.src}
-                            style={{ width: '20px' }}
-                          />
+                          <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg"><path d="M432,320H400a16,16,0,0,0-16,16V448H64V128H208a16,16,0,0,0,16-16V80a16,16,0,0,0-16-16H48A48,48,0,0,0,0,112V464a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V336A16,16,0,0,0,432,320ZM488,0h-128c-21.37,0-32.05,25.91-17,41l35.73,35.73L135,320.37a24,24,0,0,0,0,34L157.67,377a24,24,0,0,0,34,0L435.28,133.32,471,169c15,15,41,4.5,41-17V24A24,24,0,0,0,488,0Z"></path></svg>
                         </a>
                       </div>
                       <div className="card-actions justify-end"></div>
