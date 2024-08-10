82e63a3e8d14fffb2fc8aa3335a1bd28ad96c9b1
Moggach <acunnane13@gmail.com>
2024-08-10 17:48:02 +0100

    style pagination buttons

--- src/app/components/PaginatedList.js
+++ src/app/components/PaginatedList.js
@@ -18,7 +18,7 @@ const Pagination = ({ data, itemsPerPage, currentPage, setCurrentPage }) => {
       <button
         onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
         disabled={currentPage === 1}
-        className="px-4 py-2 mr-2 bg-gray-200 disabled:bg-gray-400"
+        className="px-4 py-2 mr-2 btn btn-accent  font-semibold"
       >
         Previous
       </button>
@@ -26,7 +26,7 @@ const Pagination = ({ data, itemsPerPage, currentPage, setCurrentPage }) => {
       <button
         onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
         disabled={currentPage === totalPages}
-        className="px-4 py-2 ml-2 bg-gray-200 disabled:bg-gray-400"
+        className="px-4 py-2 ml-2 btn btn-accent font-semibold "
       >
         Next
       </button>
