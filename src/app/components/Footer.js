dee17e46f116b840cd97389e3aaa60715a0a57a9
Moggach <acunnane13@gmail.com>
2024-08-10 16:45:46 +0100

    indicate links in footer with underline

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -3,8 +3,8 @@
 const Footer = () => {
     return (
         <footer className="bg-base-200 p-4">
-            <p>Made with ❤️ by <a href="https://github.com/Moggach">Moggach</a></p>
-            <p>Service isn't listed? <a href="https://airtable.com/appksbQlVr07Kxadu/pagEkSrTVCs0yk2OS/form">Submit here</a></p>
+            <p>Made with ❤️ by <a class="underline" href="https://github.com/Moggach">Moggach</a></p>
+            <p>Service isn't listed? <a class="underline" href="https://airtable.com/appksbQlVr07Kxadu/pagEkSrTVCs0yk2OS/form">Submit here</a></p>
         </footer>
     );
 };
