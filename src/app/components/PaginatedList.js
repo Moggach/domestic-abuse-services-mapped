1aba158c62da26e64d04c5087696ef7c051fc503
Moggach <acunnane13@gmail.com>
2024-08-10 17:49:07 +0100

    make button text white

--- src/app/components/PaginatedList.js
+++ src/app/components/PaginatedList.js
@@ -18,7 +18,7 @@ const Pagination = ({ data, itemsPerPage, currentPage, setCurrentPage }) => {
       <button
         onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
         disabled={currentPage === 1}
-        className="px-4 py-2 mr-2 btn btn-accent  font-semibold"
+        className="px-4 py-2 mr-2 btn btn-accent  text-white font-semibold"
       >
         Previous
       </button>
@@ -26,7 +26,7 @@ const Pagination = ({ data, itemsPerPage, currentPage, setCurrentPage }) => {
       <button
         onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
         disabled={currentPage === totalPages}
-        className="px-4 py-2 ml-2 btn btn-accent font-semibold "
+        className="px-4 py-2 ml-2 btn btn-accent font-semibold text-white"
       >
         Next
       </button>
