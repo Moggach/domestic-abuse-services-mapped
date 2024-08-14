1196901568675427c12f6aa01c201e4f74a0a609
Moggach <acunnane13@gmail.com>
2024-08-14 11:48:43 +0100

    add open graph image and meta properties

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
-import Navbar from "./components/NavBar"
-import localFont from '@next/font/local'
-
+import Navbar from "./components/NavBar";
+import localFont from '@next/font/local';
 
 export const metadata = {
   title: 'Domestic abuse services mapping',
@@ -8,8 +7,20 @@ export const metadata = {
   icons: {
     icon: '/favicon.ico',
   },
-}
-
+  openGraph: {
+    title: 'Domestic abuse services mapping',
+    description: 'A tool for mapping domestic abuse services across the UK.',
+    images: [
+      {
+        url: '/og-image.png', 
+        width: 1200,
+        height: 630,
+        alt: 'Domestic abuse services mapping',
+      },
+    ],
+    type: 'website',
+  },
+};
 
 const poppins = localFont({
   src: [
@@ -23,7 +34,7 @@ const poppins = localFont({
     }
   ],
   variable: '--font-poppins'
-})
+});
 
 const opensans = localFont({
   src: [
@@ -31,15 +42,22 @@ const opensans = localFont({
       path: '../../public/fonts/OpenSans-Regular.ttf',
       weight: '400'
     },
-    
   ],
   variable: '--font-opensans'
-})
+});
 
 export default function RootLayout({ children }) {
   return (
     <html data-theme="light" lang="en" className={`${poppins.variable} ${opensans.variable} bg-base-200`}>
       <head>
+        {/* Open Graph meta tags */}
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta property="og:description" content={metadata.openGraph.description} />
+        <meta property="og:image" content={metadata.openGraph.images[0].url} />
+        <meta property="og:image:width" content={metadata.openGraph.images[0].width} />
+        <meta property="og:image:height" content={metadata.openGraph.images[0].height} />
+        <meta property="og:image:alt" content={metadata.openGraph.images[0].alt} />
+        <meta property="og:type" content={metadata.openGraph.type} />
       </head>
       <body className="font-body h-screen">
         <Navbar />
@@ -48,5 +66,5 @@ export default function RootLayout({ children }) {
         </div>
       </body>
     </html>
-  )
+  );
 }
