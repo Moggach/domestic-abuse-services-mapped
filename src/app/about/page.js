82072f1c490b810f1fe1dca2a4abc996ca675bc1
Moggach <acunnane13@gmail.com>
2024-10-03 16:50:05 +0100

    update email address again

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -6,7 +6,7 @@ const About = () => {
     <div className="flex flex-col gap-4 text-xl">
     <p>Domestic abuse services mapped is a project built and maintained by <a className="underline" href="https://github.com/Moggach">Moggach</a></p>
     <p>If your service isn't listed please submit the details using this <a className="underline" href="https://airtable.com/appksbQlVr07Kxadu/pagEkSrTVCs0yk2OS/form">form</a> </p>
-    <p>Have a question or a comment about the site? Please email <a className="underline" href="mailto:hello@domesticabuseservices.co.uk">hello@domesticabuseservices.co.uk</a></p>
+    <p>Have a question or a comment about the site? Please email <a className="underline" href="mailto:hello@domesticabuseservices.uk">hello@domesticabuseservices.uk</a></p>
     <p>Please note that addresses of services may be approximate. Please contact any service before visiting them</p>
     </div>
 
