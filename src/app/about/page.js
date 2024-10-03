0fb2f0fe90a74b04e4b5b62e54299cefe8985197
Moggach <acunnane13@gmail.com>
2024-10-03 16:42:51 +0100

    revert change to about page text

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,7 +5,7 @@ const About = () => {
     <main className="p-4"><h1 className='font-headings text-3xl mb-4'>About</h1>
     <div className="flex flex-col gap-4 text-xl">
     <p>Domestic abuse services mapped is a project built and maintained by <a className="underline" href="https://github.com/Moggach">Moggach</a></p>
-    <p>If your service isn't listed or to make a correction please use this <a className="underline" href="https://airtable.com/appksbQlVr07Kxadu/pagEkSrTVCs0yk2OS/form">form</a> </p>
+    <p>If your service isn't listed please submit the details using this <a className="underline" href="https://airtable.com/appksbQlVr07Kxadu/pagEkSrTVCs0yk2OS/form">form</a> </p>
     <p>Have a question or a comment about the site? Please email <a className="underline" href="mailto:hello@domesticabuseservicesmapped.co.uk">hello@domesticabuseservicesmapped.co.uk</a></p>
     </div>
 
