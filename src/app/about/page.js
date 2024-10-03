a92d0c00b836d7b025c689541a01f8249e66b81f
Moggach <acunnane13@gmail.com>
2024-10-03 16:56:14 +0100

    add to about page text

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -8,6 +8,7 @@ const About = () => {
     <p>If your service isn't listed please submit the details using this <a className="underline" href="https://airtable.com/appksbQlVr07Kxadu/pagEkSrTVCs0yk2OS/form">form</a> </p>
     <p>Have a question or a comment about the site? Please email <a className="underline" href="mailto:hello@domesticabuseservices.uk">hello@domesticabuseservices.uk</a></p>
     <p>Please note that addresses of services may be approximate. Please contact any service before visiting them</p>
+    <p>Refuges are not listed for safety reasons. To access a refuge space please contact the 24/7 freephone <a className="underline" href="https://www.nationaldahelpline.org.uk/">National Domestic Abuse Helpline</a> on 0808 2000 247</p>
     </div>
 
   
