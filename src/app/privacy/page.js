cea177a83df1b9f81465e86742cb8a24931fa129
Moggach <acunnane13@gmail.com>
2024-10-04 15:59:15 +0100

    add privacy policy

--- src/app/privacy/page.js
+++ src/app/privacy/page.js
@@ -6,8 +6,17 @@ import QuickExit from '../components/QuickExit';
 const Privacy = () => {
   return (
     <>
-    <main className="p-4"><h1 className='font-headings text-3xl'>Privacy</h1></main>
-    <QuickExit/>
+      <main className="p-4"><h1 className='font-headings text-3xl mb-4'>Privacy Policy </h1>
+        <div className="flex flex-col gap-4 text-xl">
+
+          <p><em>Last Updated: 4th October 2024</em></p>
+          <p>This policy explains how this site uses <a className="underline" href='https://withcabin.com/'>Cabin</a>, a privacy-first, cookieless web analytics solution, to collect and process visitor data</p>
+            <p>Cabin is a lightweight, privacy-focused analytics tool that collects minimal data without using cookies or tracking technologies. Cabin does not store any personally identifying information</p>
+            <p>Cabin complies with privacy laws, including GDPR. It does not sell, share, or pass on any data to third parties. Visitor data remains confidential and is never used for advertising purposes</p>
+            <p>If you have questions about this policy, please contact <a className="underline" href="mailto:hello@domesticabuseservices.uk">hello@domesticabuseservices.uk</a></p>
+        </div>
+      </main>
+      <QuickExit />
     </>
   );
 };
