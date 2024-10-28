9db9a361f30d8d66d1ce0f09ee5f9255a0aeeb75
Moggach <acunnane13@gmail.com>
2024-10-28 17:20:21 +0000

    prevent footer from disappearing behind quick exit button

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -2,7 +2,7 @@
 
 const Footer = () => {
     return (
-        <footer className="p-4">
+        <footer className="p-4 pb-[55px] md:pb-4">
             <p>Made with ❤️ by <a className="underline" href="https://github.com/Moggach">Moggach</a></p>
             <p>Service isn't listed? <a className="underline" href="https://airtable.com/appksbQlVr07Kxadu/pagEkSrTVCs0yk2OS/form">Submit here</a></p>
         </footer>
