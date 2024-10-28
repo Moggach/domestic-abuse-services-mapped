bbcecce67d5aa658f4946510f3fbe4df065244a5
Moggach <acunnane13@gmail.com>
2024-10-28 17:30:49 +0000

    add some padding to bottom of about page content

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -8,7 +8,7 @@ const About = () => {
   return (
     <>
     <main className="p-4"><h1 className='font-headings text-3xl mb-4'>About</h1>
-    <div className="flex flex-col gap-4 text-xl">
+    <div className="flex flex-col gap-4 text-xl pb-[55px]">
     <p>Domestic abuse services mapped is a project built and maintained by <a className="underline" href="https://github.com/Moggach">Moggach</a></p>
     <p>If your service isn't listed please submit the details using this <a className="underline" href="https://airtable.com/appksbQlVr07Kxadu/pagEkSrTVCs0yk2OS/form">form</a> </p>
     <p>Have a question or a comment about the site? Please email <a className="underline" href="mailto:hello@domesticabuseservices.uk">hello@domesticabuseservices.uk</a></p>
