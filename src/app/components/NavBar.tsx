9dfb4c35cf21bc8f2cfe752a8f4c4c517dee853e
Moggach <acunnane13@gmail.com>
2025-01-06 16:13:22 +0000

    add link to delete browser history page in navbar and tooltips on bin icon and theme switcher

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -39,7 +39,25 @@ const NavBar: React.FC = () => {
               </Link>
             ))}
             <ThemeSwitcher />
+            <div className="tooltip tooltip-left" data-tip="Delete from browser history">
+              <Link href="/clear-browser">
+                <svg
+                  fill="currentColor"
+                  version="1.1"
+                  className="h-8 w-8 text-accent"
+                  viewBox="0 0 408.483 408.483"
+                >
+                  <g>
+                    <g>
+                      <path d="M87.748,388.784c0.461,11.01,9.521,19.699,20.539,19.699h191.911c11.018,0,20.078-8.689,20.539-19.699l13.705-289.316    H74.043L87.748,388.784z M247.655,171.329c0-4.61,3.738-8.349,8.35-8.349h13.355c4.609,0,8.35,3.738,8.35,8.349v165.293    c0,4.611-3.738,8.349-8.35,8.349h-13.355c-4.61,0-8.35-3.736-8.35-8.349V171.329z M189.216,171.329    c0-4.61,3.738-8.349,8.349-8.349h13.355c4.609,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.737,8.349-8.349,8.349h-13.355    c-4.61,0-8.349-3.736-8.349-8.349V171.329L189.216,171.329z M130.775,171.329c0-4.61,3.738-8.349,8.349-8.349h13.356    c4.61,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.738,8.349-8.349,8.349h-13.356c-4.61,0-8.349-3.736-8.349-8.349V171.329z" />
+                      <path d="M343.567,21.043h-88.535V4.305c0-2.377-1.927-4.305-4.305-4.305h-92.971c-2.377,0-4.304,1.928-4.304,4.305v16.737H64.916    c-7.125,0-12.9,5.776-12.9,12.901V74.47h304.451V33.944C356.467,26.819,350.692,21.043,343.567,21.043z" />
+                    </g>
+                  </g>
+                </svg>
+              </Link>
+            </div>
           </div>
+
 
           <div className="md:hidden text-accent">
             <button onClick={toggleDrawer} aria-label="Toggle menu">
@@ -64,18 +82,16 @@ const NavBar: React.FC = () => {
         role="button"
         tabIndex={0}
         aria-label="Close drawer overlay"
-        className={`fixed inset-0 bg-black bg-opacity-50 z-40 transition-opacity ${
-          drawerOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
-        }`}
+        className={`fixed inset-0 bg-black bg-opacity-50 z-40 transition-opacity ${drawerOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
+          }`}
         onClick={toggleDrawer}
         onKeyDown={handleKeyDown}
       ></div>
 
       {/* Drawer */}
       <div
-        className={`fixed inset-y-0 right-0 bg-base-200 w-64 z-50 transform ${
-          drawerOpen ? 'translate-x-0' : 'translate-x-full'
-        } transition-transform`}
+        className={`fixed inset-y-0 right-0 bg-base-200 w-64 z-50 transform ${drawerOpen ? 'translate-x-0' : 'translate-x-full'
+          } transition-transform`}
       >
         <div className="p-4 flex flex-col gap-4 text-accent">
           <div className="self-end">
@@ -106,6 +122,25 @@ const NavBar: React.FC = () => {
             </Link>
           ))}
           <ThemeSwitcher />
+
+          <div className="tooltip tooltip-left" data-tip="Delete from browser history">
+              <Link href="/clear-browser">
+                <svg
+                  fill="currentColor"
+                  version="1.1"
+                  className="h-8 w-8 text-accent"
+                  viewBox="0 0 408.483 408.483"
+                >
+                  <g>
+                    <g>
+                      <path d="M87.748,388.784c0.461,11.01,9.521,19.699,20.539,19.699h191.911c11.018,0,20.078-8.689,20.539-19.699l13.705-289.316    H74.043L87.748,388.784z M247.655,171.329c0-4.61,3.738-8.349,8.35-8.349h13.355c4.609,0,8.35,3.738,8.35,8.349v165.293    c0,4.611-3.738,8.349-8.35,8.349h-13.355c-4.61,0-8.35-3.736-8.35-8.349V171.329z M189.216,171.329    c0-4.61,3.738-8.349,8.349-8.349h13.355c4.609,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.737,8.349-8.349,8.349h-13.355    c-4.61,0-8.349-3.736-8.349-8.349V171.329L189.216,171.329z M130.775,171.329c0-4.61,3.738-8.349,8.349-8.349h13.356    c4.61,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.738,8.349-8.349,8.349h-13.356c-4.61,0-8.349-3.736-8.349-8.349V171.329z" />
+                      <path d="M343.567,21.043h-88.535V4.305c0-2.377-1.927-4.305-4.305-4.305h-92.971c-2.377,0-4.304,1.928-4.304,4.305v16.737H64.916    c-7.125,0-12.9,5.776-12.9,12.901V74.47h304.451V33.944C356.467,26.819,350.692,21.043,343.567,21.043z" />
+                    </g>
+                  </g>
+                </svg>
+              </Link>
+            </div>
+
         </div>
       </div>
     </>
