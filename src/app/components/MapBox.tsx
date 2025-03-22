212eff459b9b0e8048822898c09c981939a3ab96
Moggach <acunnane13@gmail.com>
2025-03-22 18:20:45 +0000

    increase size of unclustered points slightly

--- src/app/components/MapBox.tsx
+++ src/app/components/MapBox.tsx
@@ -187,7 +187,7 @@ const MapBox: React.FC<MapBoxProps> = ({
             filter: ['!', ['has', 'point_count']],
             paint: {
               'circle-color': '#11b4da',
-              'circle-radius': 4,
+              'circle-radius': 5,
               'circle-stroke-width': 1,
               'circle-stroke-color': '#fff',
             },
